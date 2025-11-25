3883e2965caa92193c5c2638296c25efe755f39a
Victor Castro <victorcastro.dev@gmail.com>
2025-11-25 08:04:53 -0300

    feat: adiciona tratamento global de exceções na API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,33 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Recurso não encontrado',
+                'error' => class_basename($e->getModel()) . ' não encontrado'
+            ], 404);
+        });
+
+        $exceptions->render(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => 'Endpoint não encontrado',
+                'error' => 'A rota solicitada não existe'
+            ], 404);
+        });
+
+        $exceptions->render(function (\InvalidArgumentException $e) {
+            return response()->json([
+                'message' => 'Dados inválidos',
+                'error' => $e->getMessage()
+            ], 400);
+        });
+
+        $exceptions->render(function (\Throwable $e) {
+            if (config('app.debug')) return null;
+
+            return response()->json([
+                'message' => 'Erro interno do servidor',
+                'error' => 'Ocorreu um erro inesperado'
+            ], 500);
+        });
     })->create();
